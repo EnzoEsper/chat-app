2ed14d96b116ac58782ca4fca18a7d3fdb2eea4d
Enzo Esperguin <enzoesper@hotmail.com>
2019-12-22 01:58:59 -0300

    sharing the location between the clients using the browser geolocation API

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,7 +2,7 @@ const socket = io();
 
 socket.on('message', (message) => {
   console.log(message);
-})
+});
 
 document.querySelector("#message-form").addEventListener('submit', (e) => {
   e.preventDefault();
@@ -10,4 +10,18 @@ document.querySelector("#message-form").addEventListener('submit', (e) => {
   const message = e.target.elements.message;
 
   socket.emit('sendMessage', message);
+});
+
+document.querySelector("#send-location").addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('geolocation is not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  })
+  
 })
