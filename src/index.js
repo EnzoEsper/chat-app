b1464c6533c9845f365d5d4b5b0893868d5eb851
Enzo Esperguin <enzoesper@hotmail.com>
2019-12-22 01:35:40 -0300

    broadcasting events whe a user joins/leave a connection

--- src/index.js
+++ src/index.js
@@ -16,10 +16,16 @@ io.on('connection', (socket) => {
   console.log("new web socket connection");
 
   socket.emit('message', "Welcome! :)");
+  socket.broadcast.emit('message', "A new user has joied");
 
   socket.on('sendMessage', (message) => {
     io.emit('message', message);
-  })
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', "A user has left!");
+  });
+  
 })
 
 server.listen(port, () => {
