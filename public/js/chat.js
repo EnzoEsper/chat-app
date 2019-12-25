755efdab279f54c37fe9549d519bf848d5a19b02
Enzo Esperguin <enzoesper@hotmail.com>
2019-12-25 12:12:38 -0300

    using rooms to connect a certain user to a specific room chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,10 @@ const $messages = document.querySelector("#messages");
 const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
+
 socket.on('message', (message) => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
@@ -70,5 +74,6 @@ $sendLocationButton.addEventListener('click', () => {
       console.log("Location shared!");
     });
   })
-  
-})
+});
+
+socket.emit('join', { username, room });
