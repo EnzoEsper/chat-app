ced61bc114a0f536376ed22b25269493f7c5f408
Enzo Esperguin <enzoesper@hotmail.com>
2019-12-25 22:02:41 -0300

    adding utility function to store the users for manage them in the room chats part1

---
+++ src/utils/users.js
@@ -0,0 +1,54 @@
+// keeping track of the users in an array
+const users = [];
+
+// addUser, removeUser, getUser, getUsersInRoom
+const addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: 'Username and room are required'
+    }
+  }
+
+  // make sure that the name is unique in the room
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username
+  });
+
+  // validate username
+  if (existingUser) {
+    return {
+      error: 'Username is in use!'
+    }
+  };
+
+  // store the user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  };
+
+};
+
+addUser({
+  id: 22,
+  username: '      Dada',
+  room: 'Life     '
+});
+
+console.log(users);
+
+const removedUser = removeUser(22);
+console.log(removedUser);
+console.log(users);
