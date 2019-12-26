53b82f570c29ad4d0d037797d551430db78f27fa
Enzo Esperguin <enzoesper@hotmail.com>
2019-12-25 22:15:14 -0300

    Adding getUser and getUsersInRoom functions to manage the users

--- src/utils/users.js
+++ src/utils/users.js
@@ -41,14 +41,18 @@ const removeUser = (id) => {
 
 };
 
-addUser({
-  id: 22,
-  username: '      Dada',
-  room: 'Life     '
-});
+const getUser = (id) => {
+  return users.find(user => user.id === id);
+};
 
-console.log(users);
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return users.filter(user => user.room === room);
+};
 
-const removedUser = removeUser(22);
-console.log(removedUser);
-console.log(users);
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
